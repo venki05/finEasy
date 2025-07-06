d1fe64b83a75d8a86f19dccc792dc126c7aaa895
venki05 <prmvenki@gmail.com>
2025-07-06 15:07:27 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/finEasy/' // 👈 Your GitHub repo name goes here
-})
+  base: "/fineasy",
+});
